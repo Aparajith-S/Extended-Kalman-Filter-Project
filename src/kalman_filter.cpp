f3b1a1a8af585e18a6c33873cceb0b6e318cb317
root <root@LAPTOP-JH86KBNJ.localdomain>
2021-04-26 02:34:06 +0200

    implement EKF algorithm

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,5 +1,7 @@
 #include "kalman_filter.h"
-
+#include "tools.h"
+#include <limits.h>
+namespace kalman{
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
@@ -8,12 +10,18 @@ using Eigen::VectorXd;
  *   VectorXd or MatrixXd objects with zeros upon creation.
  */
 
-KalmanFilter::KalmanFilter() {}
-
+KalmanFilter::KalmanFilter(VectorXd const & x_in,
+  MatrixXd const & P_in,
+  MatrixXd const & F_in):
+  x_(x_in),
+  P_(P_in),
+  F_(F_in)
+  {};
 KalmanFilter::~KalmanFilter() {}
 
-void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
-                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
+void KalmanFilter::Init(VectorXd const &x_in, MatrixXd const &P_in, MatrixXd const &F_in,
+                        MatrixXd const &H_in, MatrixXd const &R_in, MatrixXd const &Q_in) 
+{
   x_ = x_in;
   P_ = P_in;
   F_ = F_in;
@@ -23,19 +31,92 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-   * TODO: predict the state
-   */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Kalman Filter equations
-   */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  KfCommonUpdates(y);
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Extended Kalman Filter equations
-   */
+float px = x_[0];
+  float py = x_[1];
+  float vx = x_[2];
+  float vy = x_[3];
+
+  //Checking the value is not zero
+  if(px == 0. && py == 0.)
+    return;
+  
+  float rho = sqrt(px*px + py*py);
+  float theta = atan2(py, px);
+  
+  //Checking the value is not zero
+  if (rho < std::numeric_limits<float>::epsilon()) 
+  {
+    rho = std::numeric_limits<float>::epsilon() ;
+  } 
+  float rho_dot = (px*vx + py*vy) / rho;
+  
+  //Finding h(x)
+  VectorXd h = VectorXd(3); // h(x_)
+  h << rho, theta, rho_dot;
+  VectorXd y = z-h;
+
+  //Normalize the angle between -pi to pi
+  while (y[1] < -M_PI)
+    y[1] += 2 * M_PI;
+  while (y[1] > M_PI)
+    y[1] -= 2 * M_PI;
+  
+  //Common measurement update
+  KfCommonUpdates(y) ;
+}
+
+void KalmanFilter::KfCommonUpdates(const Eigen::VectorXd &y)
+{ 
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+}
+
+Eigen::MatrixXd & KalmanFilter::modifyQmatrix(void)
+{
+  return this->Q_;
+}
+Eigen::MatrixXd & KalmanFilter::modifyRmatrix(void)
+{
+  return this->R_;
+}
+Eigen::MatrixXd & KalmanFilter::modifyFmatrix(void)
+{
+  return this->F_;
+}
+Eigen::MatrixXd & KalmanFilter::modifyHmatrix(void)
+{
+  return this->H_;
+}
+Eigen::VectorXd & KalmanFilter::modifyVectorX(void)
+{
+  return this->x_;
+}
+Eigen::VectorXd KalmanFilter::getX(void)const
+{
+  return x_;
+}
+Eigen::MatrixXd KalmanFilter::getP(void)const
+{
+  return P_;
+}
 }
